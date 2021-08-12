bfff8e2fb4844f86aad267b9fa51e330d04b89a0
Vardaan <vardaan.agarwal.98@gmail.com>
2021-08-12 13:33:41 +0530

    js input

---
+++ quiz.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("#form");
+const btn = document.querySelector("#btn");
+const output = document.querySelector(".output");
+
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log();
+}
