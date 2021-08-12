a859e64ae8e843a64c2761cc6dcf5dea48a50f5e
Vardaan <vardaan.agarwal.98@gmail.com>
2021-08-12 14:00:55 +0530

    finish Yaay!!

--- quiz.js
+++ quiz.js
@@ -1,10 +1,35 @@
-const form = document.querySelector("#form");
+let form = document.querySelector("#form");
 const btn = document.querySelector("#btn");
-const output = document.querySelector(".output");
+const output = document.querySelector(".quiz-output");
+
+const ansArr = [
+  "90°",
+  "180°",
+  "(Base X Height) / 2",
+  "Equilateral triangle",
+  "3",
+];
 
 form.addEventListener("submit", onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
-  console.log();
+
+  let i = 0;
+  let score = 0;
+
+  const formResults = new FormData(form);
+
+  for (const value of formResults.values()) {
+    console.log(value);
+    if (ansArr[i] === value) {
+      score++;
+      console.log(score);
+    }
+    i++;
+    console.log(i);
+  }
+
+  output.innerHTML = `Your Score Is ${score}`;
+  output.style.width = "200px";
 }
